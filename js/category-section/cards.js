0c4af330d15fc34b0be77382d1ea659945601215
s6eo <mohammed.a96h@gmail.com>
2025-06-01 19:00:41 +0300

    category

--- js/category-section/cards.js
+++ js/category-section/cards.js
@@ -7,28 +7,34 @@ export default function card(items){
     const htmlCode =
         `
         <div class="card Scard">
+
                 <div class="L-R-Arrow flex flex-item-center gap-10">
                     <div class="cursor"><img src="img/icon/L-arrow.svg" alt="left arrow"></div>
-                    <div class="cursor"><img src="img/icon/R-arrow.svg" altright arrowdiv>
+                    <div class="cursor"><img src="img/icon/R-arrow.svg" altright arrowdiv></div>
                 </div>
-            <div class="G-img" style=" background: url(${game_img}) ; background-position: center; background-size: cover; "></div>
-            <div class="details flex flex-column gap-20">
-                <div class="title flex flex-column gap-6">
 
-                    <div calss="buttoms-sm" style="display: flex; gap:10px; ">
-                        <div class="btn-sm">${btn1}</div>
-                        <div class="btn-sm">${btn2}</div>
+                <div class="G-img" style=" background: url(${game_img}) ; background-position: center; background-size: cover;"></div>
+
+                <div class="details flex flex-column gap-20">
+
+                    <div class="title flex flex-column gap-6">
+
+                        <div calss="buttoms-sm" style="display: flex; gap:10px;">
+                            <div class="btn-sm">${btn1}</div>
+                            <div class="btn-sm">${btn2}</div>
+                        </div>
+
+                        <h6>${title}</h6>
+
+                        <div class="views flex flex-item-center gap-10">
+                            <div class="point"></div>
+                            <p>${views}</p>
+                        </div>
                     </div>
 
-                    <h6>${title}</h6>
-
-                    <div class="views flex flex-item-center gap-10">
-                        <div class="point"></div>
-                        <p>${views}</p>
-                    </div>
+                    <button class="btn"> Follow</button>
+                    
                 </div>
-                <button class="btn"> Follow</button>
-            </div>
         </div>
         `
         cardsComponint.insertAdjacentHTML('afterbegin', htmlCode);
